76d277f97837d28982ee32f985b4aa0eb9c3130c
Difroy <unifroy@gmail.com>
2024-05-28 14:27:37 +0200

    Update 0.0.2
    Creazione classe UserType e conclusione del model user.

--- src/main/java/bnb/pulse/model/User.java
+++ src/main/java/bnb/pulse/model/User.java
@@ -1,10 +1,11 @@
 package bnb.pulse.model;
-import org.hibernate.annotations.Cascade;
-import org.springframework.format.annotation.DateTimeFormat;
 
+import bnb.pulse.model.UserType;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,18 +13,79 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
-
+@SuppressWarnings("unused")
 @Entity
-@Table (name = "users")
+@Table(name = "users")
 public class User {
 
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "id_profile", referencedColumnName = "id")
 	private Profile profile;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "user_type", nullable = false)
+	private UserType userType;
+
+	@Column(name = "username", nullable = false, length = 40)
+	private String username;
+
+	@Column(name = "email", nullable = false, length = 50)
+	private String email;
+
+	@Column(name = "password", nullable = false, length = 50)
+	private String password;
+
 	
-	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
+	public UserType getUserType() {
+		return userType;
+	}
+
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
---
+++ src/main/java/bnb/pulse/model/UserType.java
@@ -0,0 +1,11 @@
+package bnb.pulse.model;
+
+public enum UserType {
+
+	HOST,
+    GUEST,
+    ADMIN,
+    STAFF,
+    NONE; // Aggiunto per rappresentare il valore ''
+	
+}
