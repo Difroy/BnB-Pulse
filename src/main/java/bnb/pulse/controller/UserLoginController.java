bf4e8a5ada867e8efa8da9c85c5466da6e887ef9
Difroy <unifroy@gmail.com>
2024-05-29 12:48:22 +0200

    Update 0.1.3
    Completamento loginController

--- src/main/java/bnb/pulse/controller/UserLoginController.java
+++ src/main/java/bnb/pulse/controller/UserLoginController.java
@@ -2,9 +2,14 @@ package bnb.pulse.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import bnb.pulse.model.User;
 import bnb.pulse.service.UserService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/login")
@@ -12,5 +17,23 @@ public class UserLoginController {
 	@Autowired
 	private UserService userService;
 	
+	public String getPage (Model model, HttpSession session, @RequestParam(name = "error", required = false) String error) {
+		User user = (User) session.getAttribute("utente");
+		model.addAttribute("user", user);
+		
+		if(session.getAttribute("user") != null)
+			return "redirect:/";
+			model.addAttribute("error", error);
+			return "login";
+	}
+	@PostMapping
+	public String formAccess(@RequestParam("username")String username,
+							 @RequestParam("password")String password,
+							 HttpSession session) {
+		if (!userService.loginUtente(username, password, session))
+			return "redirect:/login";
+		return "redirect:/";
+		
+	}
 	
 }
