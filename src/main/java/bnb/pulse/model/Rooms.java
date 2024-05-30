5b2faac472d50776ff67879780273f83d285839a
Difroy <unifroy@gmail.com>
2024-05-31 00:43:12 +0200

    Update 0.1.9
    Creazione di rooms.model

---
+++ src/main/java/bnb/pulse/model/Rooms.java
@@ -0,0 +1,96 @@
+package bnb.pulse.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "rooms")
+public class Rooms {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_propertie", nullable = false)
+    private Properties properties;
+
+    @Column(name = "room_type", nullable = false)
+    private int roomType;
+
+    @Column(name = "title", nullable = false)
+    private int title;
+
+    @Column(name = "description")
+    private int description;
+
+    @Column(name = "price_per_night", nullable = false)
+    private int pricePerNight;
+
+    @Column(name = "max_guest", nullable = false)
+    private int maxGuest;
+
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Properties getProperty() {
+        return properties;
+    }
+
+    public void setProperty(Properties properties) {
+        this.properties = properties;
+    }
+
+    public int getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(int roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getTitle() {
+        return title;
+    }
+
+    public void setTitle(int title) {
+        this.title = title;
+    }
+
+    public int getDescription() {
+        return description;
+    }
+
+    public void setDescription(int description) {
+        this.description = description;
+    }
+
+    public int getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(int pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public int getMaxGuest() {
+        return maxGuest;
+    }
+
+    public void setMaxGuest(int maxGuest) {
+        this.maxGuest = maxGuest;
+    }
+}
