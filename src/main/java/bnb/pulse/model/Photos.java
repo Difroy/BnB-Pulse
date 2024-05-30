03393b3b680e885422e6dd45c02575727dac02fa
Difroy <unifroy@gmail.com>
2024-05-31 00:53:07 +0200

    Update 0.2.0
    Creazione Photos.model

---
+++ src/main/java/bnb/pulse/model/Photos.java
@@ -0,0 +1,52 @@
+package bnb.pulse.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "photos")
+public class Photos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_room", nullable = false)
+    private Rooms rooms;
+
+    @Column(name = "URL", columnDefinition = "LONGTEXT", nullable = false)
+    private String url;
+
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Rooms getRoom() {
+        return rooms;
+    }
+
+    public void setRoom(Rooms rooms) {
+        this.rooms = rooms;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
