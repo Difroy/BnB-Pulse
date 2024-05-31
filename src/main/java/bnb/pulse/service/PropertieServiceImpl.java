35d14c0098a095a88e9b1dbe6913ba77ee4610a0
Difroy <unifroy@gmail.com>
2024-05-31 02:59:50 +0200

    Update 0.2.3
    Quasi completamente del serviceImpl di Propertie.

--- src/main/java/bnb/pulse/service/PropertieServiceImpl.java
+++ src/main/java/bnb/pulse/service/PropertieServiceImpl.java
@@ -1,12 +1,15 @@
 package bnb.pulse.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import java.util.List;
+
 import bnb.pulse.dao.PropertieDao;
 import bnb.pulse.model.Propertie;
 import jakarta.servlet.http.HttpSession;
@@ -31,35 +34,71 @@ public class PropertieServiceImpl implements PropertieService {
 			return optionalPropertie.get();
 		return null;
 	}
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean reserve(int idPropertie, HttpSession session) {
 		Propertie propertie = getPropertieById (idPropertie);
 		List <Propertie> booking;
-		if (session.getAttribute("Booking")!=null) {
-			booking = (List <Propertie>)
+		
+		if (session.getAttribute("booking")!=null) {
+			booking = (List <Propertie>) session.getAttribute("reserve");
+			for (Propertie p : booking)
+				if (p.getId() == propertie.getId())
+					return false;
+		
+		booking.add(propertie);
+		session.setAttribute("booking", booking);
+		return true;
+		}else {
+			booking = new ArrayList <>();
+			booking.add(propertie);
+			session.setAttribute("booking", booking);
+			return true;
 		}
-		return false;
 	}
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Propertie> getReserve(HttpSession session) {
-		// TODO Auto-generated method stub
+		if (session.getAttribute("booking") != null) {
+			return (List<Propertie>) session.getAttribute("booking");
+		}
 		return null;
 	}
+	@SuppressWarnings("unchecked")
 	@Override
 	public void deleteReserve(int idPropertie, HttpSession session) {
-		// TODO Auto-generated method stub
+
+		List <Propertie> reserve = (List<Propertie>) session.getAttribute("reserve");
+		for (Propertie p: reserve)
+			if (p.getId()==idPropertie) {
+				reserve.remove(p);
+				break;
+			}
+		
+		reserve = reserve.stream().filter(p -> p.getId() != idPropertie).collect(Collectors.toList());
+		if (reserve.size()>0)
+			session.setAttribute("reserve", reserve);
+		else
+			session.removeAttribute("booking");
 		
 	}
+	@SuppressWarnings("unchecked")
 	@Override
 	public double getPriceReserve(HttpSession session) {
-		// TODO Auto-generated method stub
+		List <Propertie> reserve = (List<Propertie>) session.getAttribute("reserve");
+		if (reserve == null) {
+			
+			@SuppressWarnings("null")
+			double total = reserve.stream().mapToDouble(Propertie::getPricePerNight).reduce(0, Double::sum);
+					return total;
+		}
 		return 0;
 	}
 	@Override
 	public void savePropertie(Propertie propertie, int idUser, MultipartFile coverPhoto, String title,
 			String description, String address, String area, String city, String country, String pricePerNight,
 			String maxGuest) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 	@Override
